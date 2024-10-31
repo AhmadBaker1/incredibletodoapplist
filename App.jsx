02317577328f3e8f60aa061cacd6963c5405055f
Ahmad Baker <ahmadqadoomy79@gmail.com>
2024-10-31 15:22:53 -0600

    added usestate to the to do list

---
+++ App.jsx
@@ -0,0 +1,40 @@
+/**
+ * My To Do List App
+ *
+ * @format
+ */
+
+import { useState } from 'react';
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  Pressable,
+  View,
+  Text,
+  ScrollView,
+  TextInput,
+  Button
+} from 'react-native';
+
+import ToDoList from './ToDoList';
+import ToDoForm from './ToDoForm';
+
+
+function App() {
+
+  const [tasks, setTasks] = useState([
+    'Do laundry',
+    'Go to gym',
+    'Walk dog'
+  ]);
+
+  return (
+    <SafeAreaView>
+      <ToDoList tasks={tasks}/>
+      <ToDoForm />
+    </SafeAreaView>
+  );
+}
+
+export default App;
